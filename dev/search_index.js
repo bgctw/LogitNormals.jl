7ddd716f60552ef72d8c69abfe7314557658524e
Documenter.jl <documenter@juliadocs.github.io>
2021-12-30 16:29:24 +0000

    build based on 7d0ace0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LogitNormals","category":"page"},{"location":"#LogitNormals","page":"Home","title":"LogitNormals","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LogitNormals.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LogitNormals]","category":"page"}]
+}
